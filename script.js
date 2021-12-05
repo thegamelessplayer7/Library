16ff8f5fa9d4807eadfc5f2dad61f7ca4a8c742b
thegamelessplayer7 <thegamelessplayer7@gmail.com>
2021-12-04 18:44:18 -0600

    Changed button type to prevent duplicate of submit button

--- script.js
+++ script.js
@@ -22,4 +22,17 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     const book = new Book(event.target.title.value, event.target.author.value, event.target.pages.value, event.target.read.value)
     myLibrary.push(book);
+    displayCard(myLibrary);
 })
+
+const bodyDiv = document.getElementsByClassName('body-div');
+const para = document.getElementById('my-books')
+function displayCard(array) {
+    for(i = 0; i < array.length; i++) {
+        for (let x in array[i]) {
+        para.textContent += array[i][x];
+        }
+    }
+}
+
+// Create card and append it to bodyDiv element
