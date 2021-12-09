1eb0b787c47bea68b008e4573f6ef71bffb286d7
thegamelessplayer7 <thegamelessplayer7@gmail.com>
2021-12-08 19:33:09 -0600

    removed list button that no longer served any purpose

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-let book = '';
 
 function Book(title, author, pages, hasRead) {
     this.title = title;
@@ -35,32 +34,30 @@ form.addEventListener('submit', (e) => {
     const pages = e.target.pages.value;
     const hasRead = e.target.read.value;
 
-    book = new Book(title, author, pages, hasRead);
-    console.log(book.pages);
+    let book = new Book(title, author, pages, hasRead);
     myLibrary.push(book);
 
-    let cardDiv = document.createElement('div');
+    const cardDiv = document.createElement('div');
     bodyDiv.appendChild(cardDiv).className = 'card-div';
-    let cardInsideDiv = document.createElement('div');
+    const cardInsideDiv = document.createElement('div');
     cardDiv.appendChild(cardInsideDiv).className = 'card-inside-div';
-    let titleTag = document.createElement('h2');
+    const titleTag = document.createElement('h2');
     cardInsideDiv.appendChild(titleTag);
-    let authorTag = document.createElement('p');
+    const authorTag = document.createElement('p');
     cardInsideDiv.appendChild(authorTag);
-    let pagesTag = document.createElement('p');
+    const pagesTag = document.createElement('p');
     cardInsideDiv.appendChild(pagesTag);
-    let hasReadTag = document.createElement('p');
+    const hasReadTag = document.createElement('p');
     cardInsideDiv.appendChild(hasReadTag);
-    let toggleInfo = document.createElement('p');
+    const toggleInfo = document.createElement('p');
     cardInsideDiv.appendChild(toggleInfo);
     toggleInfo.textContent = '(click above to change the read status)';
     toggleInfo.className = 'toggle-info';
-    let deleteBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
     cardInsideDiv.appendChild(deleteBtn);
     deleteBtn.innerHTML = 'Delete';
 
     deleteBtn.addEventListener('click', () => {
-        console.log('delete');
         cardDiv.style.display = 'none';
     })
 
@@ -68,16 +65,15 @@ form.addEventListener('submit', (e) => {
         
         for(let x in array) {
             if(x === 'title') {
-                console.log(array[x]);
                 titleTag.textContent = (array[x]);
             }
-            if (x === 'author') {
+            else if (x === 'author') {
                 authorTag.textContent = (array[x]);
             }
-            if (x === 'pages') {
+            else if (x === 'pages') {
                 pagesTag.textContent = `${(array[x])} pages`;
             }
-            if (x === 'hasRead') {
+            else if (x === 'hasRead') {
                 hasReadTag.textContent = (array[x]);
             }
         }
